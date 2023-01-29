b1ef7dd86da57681ca9ea6b0e587fe540763446f
kirazero17 <longdragon02@gmail.com>
2023-01-29 21:06:42 +0700

    Right

--- app/src/main/java/GradleTest01/App.java
+++ app/src/main/java/GradleTest01/App.java
@@ -5,7 +5,7 @@ package GradleTest01;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! Yo !";
     }
 
     public static void main(String[] args) {
